0a77df388c1d59ea52d9489e25aeb84b6f4397e2
George Ciurescu <shubst3r@gmail.com>
2020-10-07 22:17:20 +0100

    first commit

---
+++ src/build/precache-manifest.ab847e066054beb79da90dda129935dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e96a027eeeaca7d2b88c252141e2169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab2b229aeb3c1606b1fc",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2b229aeb3c1606b1fc",
+    "url": "/static/js/main.0eac92f7.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
